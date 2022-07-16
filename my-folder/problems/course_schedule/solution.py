c9f9223fb2c7670021b0a543d81bd008ee99b402
Mahir Rahman <rahmanm7@miamioh.edu>
2022-07-16 19:00:58 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- my-folder/problems/course_schedule/solution.py
+++ my-folder/problems/course_schedule/solution.py
@@ -1,34 +1,16 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        d = {i: [] for i in range(numCourses)}
-        for i in range(len(prerequisites)):
-                d[prerequisites[i][0]].append(prerequisites[i][1])
-        visited = set()
-        for key in d.keys():
-            if key not in visited:
-                if self.dfs(key, d, visited, []):
-                    return False
-        return True
-        
-            
-            
-    
-    def dfs(self, i, d, visited, stk):
-        if i in visited:
-            if i in stk:
-                return True
-            else:
-                return False
-        visited.add(i)
-        stk.append(i)
-        for edge in d[i]:
-            if self.dfs(edge, d, visited, stk):
-                return True
-        stk.pop()
-        return False
-        
-                
-            
-        
-        
-
+        lst = []
+        d = {i :[] for i in range(numCourses)}
+        degree = [0] * numCourses
+        for i,j in prerequisites:
+            d[i].append(j)
+            degree[j] += 1
+        queue = collections.deque([index for index, i in enumerate(degree) if i == 0])
+        while queue:
+            node = queue.popleft()
+            for edge in d[node]:
+                degree[edge] -= 1
+                if degree[edge] == 0:
+                    queue.append(edge)
+        return sum(degree) == 0
