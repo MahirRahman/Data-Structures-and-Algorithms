70346c4ebfe5d3c93f18b919d4d388ef972643b0
Mahir Rahman <rahmanm7@miamioh.edu>
2022-08-28 04:10:39 +0000

    Sync LeetCode submission - Move Zeroes (python3)

---
+++ my-folder/problems/move_zeroes/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        fast = 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            if nums[slow] != 0:
+                slow += 1
+            fast += 1
